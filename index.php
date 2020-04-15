9228a30cbfdea0e4b83abe1cb1e74261059caefb
abc <abc@example.com>
2020-04-15 13:18:31 +0530

    4-15

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
  * Time: 9:56 AM
  */
 ?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -13,9 +14,56 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assest/css/style.css?v=<?php echo time() ?>">
+    <link href="https://fonts.googleapis.com/css?family=PT+Serif&display=swap" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
+<div class="container">
+    <div class="nav">
 
+        <ul class="nav-list">
+            <li><img src="assest/images/Apple-logo-black-and-white-429x500.png" alt="" class="logo-image"></li>
+            <li>Service Centers
+                <ul class="sub-ul">
+                    <li class="sub-li">Heart Center</li>
+                    <li class="sub-li">Spine Center</li>
+                    <li class="sub-li">Stork Center</li>
+                    <li class="sub-li">Cancer Center</li>
+                    <li class="sub-li">Cosmetic Center</li>
+                    <li class="sub-li">Bone Marrow Transplant Center</li>
+                </ul>
+            </li>
+            <li>Laboratory Services
+                <ul class="sub-ul">
+                    <li class="sub-li">Laboratory Home</li>
+                </ul>
+            </li>
+            <li>Consultations
+                <ul class="sub-ul">
+                    <li class="sub-li">OPD Consultation</li>
+                    <li class="sub-li">IPD Consultation</li>
+                </ul>
+            </li>
+
+        </ul>
+    </div>
+    <div class="slide">
+        sds
+
+    </div>
+
+    <div class="cards">
+        sdf
+    </div>
+    <div class="footerServices">
+        sf
+    </div>
+    <div class="footer">
+        dfgd
+    </div>
+
+
+</div>
 </body>
 </html>
